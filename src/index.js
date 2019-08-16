0c927c2a8135ec6e44c8410c7d840148143dade0
Brienyll <Brienyll29@gmail.com>
2019-08-15 18:50:13 -0700

    passed state as props

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SeasonDisplay from './SeasonDisplay'
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errMessage: '' };
 
-    this.state = { lat: null, errMessage: '' };
-  }
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
       position => this.setState({ lat: position.coords.latitude}),
@@ -20,7 +18,7 @@ class App extends React.Component {
     }
     
     if (!this.state.errMessage && this.state.lat) {
-      return <div>latitude: {this.state.lat}</div>
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
     return <div>Loading!</div>
